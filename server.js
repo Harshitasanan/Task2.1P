fc52c77c3d8fb7dff193c7a974a35d40bfb5a9fb
Harshitasanan <132455967+Harshitasanan@users.noreply.github.com>
2023-08-06 17:42:29 +0530

    Update server.js

--- server.js
+++ server.js
@@ -28,14 +28,9 @@ app.post('/',(req,res)=>{
         }]
     }
 
-    //  apiKey="a4b224b3c2030b24e23308c2fd7b7cdc-us21"
-
-// list_id="65987d9a1e"
-    const url = "https://us13.api.mailchimp.com/3.0/lists/b857bbc11d"
-    // https://us21.admin.mailchimp.com/
     const options={
         method:"POST",
-        auth:"diya:acac0a94fdec0c9a17efd697cb0a4dd8-us13"
+        
     }
 
     const request = https.request(url,options,function(response){
